ff835908aac06b38c976098897d4eb1f32322b8b
Déw <54241620+Dew-Hub@users.noreply.github.com>
2023-04-10 02:51:53 +0300

    Update kameratakip.cs

--- Scripts/kameratakip.cs
+++ Scripts/kameratakip.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace Cainos.PixelArtTopDown_Basic
 {
-    //let camera follow target
     public class CameraFollow : MonoBehaviour
     {
         public Transform target;
